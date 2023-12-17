befc83937082c95cbf7725eee95ab850d32a5336
CarlosribesDev <carlosribes.dev@gmail.com>
2023-12-17 20:13:45 +0100

    feat: estado de menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,10 @@
 import NavBar from "./navBar/navBar";
-import AsideNavBar from "./asideNavBar/asideNavBar";
+import SideBar from "./sideNavBar/sideNavBar";
 import Footer from "./footer/footer";
 import Header from "./header/header";
 import TableActions from "./tableActions/tableActions";
 import DataTable from "./dataTable/dataTable";
 
-export { NavBar, AsideNavBar, Footer, Header, TableActions, DataTable };
+export { NavBar, SideBar, Footer, Header, TableActions, DataTable };
 
 
--- src/components/sideNavBar/sideNavBar.tsx
+++ src/components/sideNavBar/sideNavBar.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useContext } from 'react'
 import MenuItem from './components/menuItem'
 import { FaHome } from 'react-icons/fa';
 import { IoIosSettings } from "react-icons/io";
@@ -6,6 +7,7 @@ import { GiElectric } from "react-icons/gi";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai"
 import { FcSupport } from "react-icons/fc";
 import { IconType } from 'react-icons';
+import { GlobalContext } from '@/context/globalContex';
 
 interface MenuItemData {
     route: string,
@@ -24,9 +26,12 @@ const menuItems2: MenuItemData[] = [
     {route: '/support', title: 'Support', icon: FcSupport},
 ]
 
-export default function AsideNavBar() {
+export default function SideNavBar() {
+
+    const { isOpen, toggleMenu } = useContext(GlobalContext);
+
     return (
-        <aside id="sidebar" className="fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 hidden w-64 h-full pt-16 font-normal duration-75 lg:flex transition-width" aria-label="Sidebar">
+        <aside id="sidebar" className={`fixed top-0 left-0 z-20 flex flex-col flex-shrink-0 w-64 h-full pt-16 font-normal duration-75 transition-width ${isOpen ? 'block' : 'hidden'} lg:flex`} aria-label="Sidebar">
             <div className="relative flex flex-col flex-1 min-h-0 pt-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                 <div className="flex flex-col flex-1 pt-5 pb-28 overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-rounded-[0.1667rem] scrollbar-thumb-slate-200 scrollbar-track-gray-400 dark:scrollbar-thumb-slate-900 dark:scrollbar-track-gray-800">
                     <div className="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
