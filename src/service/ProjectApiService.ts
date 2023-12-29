ef85c21b67a59832d9858e1ea5a8637983fd3a62
CarlosribesDev <carlosribes.dev@gmail.com>
2023-12-29 14:12:46 +0100

    feat: fix url

--- src/service/ProjectApiService.ts
+++ src/service/ProjectApiService.ts
@@ -4,7 +4,7 @@ import ApiService from "./ApiService";
 class ProjectApiService extends ApiService<Project> {
 
     constructor() {
-        super('https:/admin-back-production-bc03.up.railway.app/api/v1/projects');
+        super('https://admin-back-production-bc03.up.railway.app/api/v1/projects');
     }
 }
 
